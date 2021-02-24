de340ff53f930a17ada2f46fcc2861a76a982d07
Peter Barclay <peter@peter-barclay.com>
2021-02-24 19:24:37 +0000

    Change return value from CDFeedImage.toLocalFeedImage to be non-optional LocalFeedImage

--- FeedStoreChallenge/CoreDataFeedStore/Model/CDFeedImage+Helpers.swift
+++ FeedStoreChallenge/CoreDataFeedStore/Model/CDFeedImage+Helpers.swift
@@ -6,7 +6,7 @@ import Foundation
 import CoreData
 
 extension CDFeedImage {
-	func toLocalFeedImage() -> LocalFeedImage? {
+	func toLocalFeedImage() -> LocalFeedImage {
 		return LocalFeedImage(
 			id: id,
 			description: imageDescription,
